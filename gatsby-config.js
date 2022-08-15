2fad5c5a0cd43ccb9363ab0346b60c11e6f35700
Kachisa D.N <kachdekan@gmail.com>
2022-08-15 21:46:07 +0300

    initial commit and menu

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    title: `Gatsby wordpress cakeit`,
+    description: `This is a Cakeit till you make it website`,
+    author: `kachdekan`,
+    siteUrl: `https://cakeit.local/`,
   },
   plugins: [
     `gatsby-plugin-image`,
@@ -29,6 +29,21 @@ module.exports = {
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
+
+    },
+    {
+      resolve: `gatsby-source-wordpress`,
+      options: {
+        url: `http://cakeit.local/graphql`,
+      }
+    },
+    `gatsby-plugin-styled-components`,
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [`Teko\:200, 400, 500, 600, 700`],
+        display: `swap`,
+      }
     },
   ],
 }
