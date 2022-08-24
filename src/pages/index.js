72a334f711ffba911a2141d018e29d23cbecbd7a
Kachisa D.N <kachdekan@gmail.com>
2022-08-24 17:00:05 +0300

    Added the latest blog post to homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,14 @@ import Layout from '../components/Layout/Layout'
 import Hero from "../components/Hero/Hero"
 import Seo from "../components/SEO/SEO"
 import CTAArea from "../components/CTAArea/CTAArea"
+import LatestBlogPost from "../components/LatestBlogPost/LatestBlogPost"
 
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
     <Hero />
     <CTAArea />
+    <LatestBlogPost />
   </Layout>
 )
 
